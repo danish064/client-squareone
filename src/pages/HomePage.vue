d3fa7f0bdf9cc6a53afeafe9548b25adaf386bef
Danish <danish.danish.danish2016@gmail.com>
2023-05-30 01:27:49 +0500

    added hero

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,28 +1,25 @@
 <template>
-  <div class="max-w-[1200px] mx-auto mb-10 py-10 text-5xl font-medium">
+  <!-- <div class="max-w-[1200px] mx-auto mb-10 py-10 text-5xl font-medium">
     Let us manage your properties
+  </div> -->
+
+  <div id="hero">
+    <!-- Welcome to -->
+    <div class="text-[70px] font-medium">SquareOne</div>
+    <div class="pb-10">Your Premier Property Dealing Company</div>
   </div>
-  <div class="max-w-[1200px] mx-auto">
-    <div class="text-3xl font-medium mb-10">Why SquareOne?</div>
+
+  <div class="max-w-[1200px] mx-auto py-10">
     <div class="flex gap-x-10 justify-between">
       <div class="w-1/2">
-        We are the best
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-        doloribus rem omnis rerum repellendus minima iste fuga ipsam quaerat
-        necessitatibus dolor, consequuntur exercitationem quod. Cupiditate illum
-        cum quibusdam distinctio labore. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatibus, eligendi ipsam quia id dolorum sint
-        vitae doloribus ab maxime, voluptas blanditiis similique voluptatum,
-        perferendis possimus nostrum ex earum architecto iure.
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio et minus,
-        dolore ab laborum ea natus tempora id dolores, corporis sint vero
-        consequatur maiores iusto iure beatae blanditiis numquam laboriosam?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        perferendis reiciendis illum dolorem, sunt, voluptas cupiditate quidem
-        impedit doloremque sit corporis? Exercitationem impedit odio dolores,
-        consectetur pariatur rem numquam explicabo!
+        <div class="text-3xl font-medium mb-10">Why SquareOne?</div>
+        We pride ourselves on being the best in the business of real estate. As
+        a premier property dealing company, we excel in guiding clients through
+        buying, selling, and investing. Our team offers personalized solutions,
+        comprehensive services, and an extensive network of professionals. With
+        a commitment to transparency and delivering exceptional customer
+        satisfaction, choose SquareOne for an unparalleled property journey.
+        Welcome to SquareOne, where excellence is our standard.
       </div>
       <img src="https://placehold.co/400x300" alt="" />
     </div>
@@ -82,4 +79,11 @@ import ContactForm from "@/components/ContactForm.vue";
 const fzooldata =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dignissimos voluptate, necessitatibus dolorum, quisquam quasi magnam aspernatur laborum eaque blanditiis non tempora ipsum ratione quidem odit quis omnis molestias id.";
 </script>
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+#hero {
+  @apply flex flex-col justify-center;
+  @apply text-center;
+  @apply min-h-[500px] mx-auto;
+  @apply bg-black text-white;
+}
+</style>
