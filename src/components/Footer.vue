9e8f015a3c63ec42cf1cfec032943b87f0a17774
Danish <danish.danish.danish2016@gmail.com>
2023-05-31 00:28:44 +0500

    touchups

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,11 @@
   <div
     class="w-full h-20 text-white flex justify-center items-center bg-[#252628]"
   >
-    Square One
+    <div class="max-w-[1440px] w-full flex justify-between">
+      <div>&copy;SquareOne. All rights reserved</div>
+      <div class="pr-[180px] text-2xl">Square One</div>
+      <div class="font-aneklatin">Since 1995</div>
+    </div>
   </div>
 </template>
 <script setup></script>
