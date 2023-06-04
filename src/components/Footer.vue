2b9ace07c5bc5a4de6c0f641e64d5d6e7ab9de2f
Danish <danish.danish.danish2016@gmail.com>
2023-06-04 15:23:47 +0500

    modded footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div
-    class="w-full h-20 text-white flex justify-center items-center bg-[#252628]"
-  >
-    <div class="max-w-[1440px] w-full flex justify-between">
+  <div class="w-full h-24 text-white  bg-[#252628]">
+    <div class="max-w-[1440px] h-full w-full mx-auto flex justify-between items-center">
       <div>&copy;SquareOne. All rights reserved</div>
-      <div class="pr-[180px] text-2xl">Square One</div>
+      <div class="pr-[180px] text-2xl flex items-center gap-x-2">
+        Square One <img src="@/assets/SO-logo.svg" width="70" alt="" />
+      </div>
       <div class="font-aneklatin">Since 1995</div>
     </div>
   </div>
